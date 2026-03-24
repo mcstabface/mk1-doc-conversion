6087787d0fffcf18d92854559ab5d3e96deac7da
Andy Bruder <andrewbruder@yahoo.com>
2026-03-24 12:20:34 -0400

    cleaning debug artifacts, is now ready for wider distro. build 12

--- main.py
+++ main.py
@@ -30,8 +30,6 @@ runtime_root = get_runtime_root()
 
 bundle_root = get_runtime_root()
 
-print("MK1 BUILD MARKER: 2026-03-24-schema-fix-12")
-
 if getattr(sys, "frozen", False):
     work_root = Path(sys.executable).parent
 else:
